bfc90422a06d40de2f872d5d052e9a77ca1fdfe6
Edwin <edwinmoreno77@hotmail.com>
2022-05-06 14:45:32 -0400

    cambios

--- webpack.prod.js
+++ webpack.prod.js
@@ -48,42 +48,6 @@ module.exports = {
             }
         ],
 
-        generator: {
-            asset: {
-                // Generator options for asset modules
-
-                // Customize publicPath for asset modules, available since webpack 5.28.0
-                publicPath: 'https://cdn/assets/',
-
-                // Emit the asset in the specified folder relative to 'output.path', available since webpack 5.67.0
-                outputPath: 'cdn-assets/',
-            },
-            'asset/inline': {
-                // Generator options for asset/inline modules
-            },
-            'asset/resource': {
-                // Generator options for asset/resource modules
-
-                // Customize publicPath for asset/resource modules, available since webpack 5.28.0
-                publicPath: 'https://cdn/assets/',
-
-                // Emit the asset in the specified folder relative to 'output.path', available since webpack 5.67.0
-                outputPath: 'cdn-assets/',
-            },
-            javascript: {
-                // No generator options are supported for this module type yet
-            },
-            'javascript/auto': {
-                // ditto
-            },
-            'javascript/dynamic': {
-                // ditto
-            },
-            'javascript/esm': {
-                // ditto
-            },
-            // others…
-        },
     },
 
     optimization: {
